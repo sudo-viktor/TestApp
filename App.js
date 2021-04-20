43d1242c2b1fac16cd1a6f85bd6d2c5975774f54
sudo-viktor <dzkvser@gmail.com>
2021-04-20 17:40:33 +0300

    Step 1
    Create navbar and text input

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Navbar } from "./src/Navbar";
+import { AddTodo } from "./src/AddTodo";
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (title) => {
+    // const newTodo = {
+    //   id: Date.now().toString(),
+    //   title: title,
+    // };
+
+    // setTodos((prevTodos) => {
+    //   return [...prevTodos, newTodo];
+    // });
+
+    setTodos((prev) => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title: title,
+      },
+    ]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Navbar title="Todo App" />
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo} />
+
+        <View></View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 30,
   },
 });
